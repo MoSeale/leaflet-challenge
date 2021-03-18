dd64be9b04117ce2ad95048599ff00fbcd64b660
MoSeale <57756845+MoSeale@users.noreply.github.com>
2021-03-17 22:26:24 -0400

    Summary
    Define map layers, colour function for markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -12,20 +12,93 @@ d3.json(queryUrl, function(data) {
 
 function createFeatures(earthquakeData) {
     console.log(earthquakeData);
-// // Define a function we want to run once for each feature in the features array
-// // Give each feature a popup describing the place and time of the earthquake
-// function onEachFeature(feature, layer) {
-//     layer.bindPopup("<h3>" + feature.properties.place +
-//     "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-// }
-
-// // Create a GeoJSON layer containing the features array on the earthquakeData object
-// // Run the onEachFeature function once for each piece of data in the array
-// var earthquakes = L.geoJSON(earthquakeData, {
-//     onEachFeature: onEachFeature
-// });
-
-// // Sending our earthquakes layer to the createMap function
-// createMap(earthquakes);
+// Define a function we want to run once for each feature in the features array
+// Give each feature a popup describing the place and time of the earthquake
+function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+    "</h3><hr><p>" + new Date(feature.properties.time) + "</p>"
+    + "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p>"
+    + "</h3><hr><p>Depth: " + feature.geometry.coordinates[2] + "</p>");
 }
 
+// Function that will determine the color of a marker based on the magnitude of the earthquake by  size and and depth of the earthquake
+function chooseColor(depth) {
+    switch (depth) {
+    case depth<10:
+      return "green";
+    case depth<30:
+      return "lightgreen";
+    case depth<50:
+      return "darkyellow";
+    case depth<70:
+      return "orange";
+    case depth<90:
+      return "darkorange";
+    default:
+      return "red";
+    }
+  }
+
+// Create a GeoJSON layer containing the features array on the earthquakeData object
+// Run the onEachFeature function once for each piece of data in the array
+var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+
+
+    
+
+
+
+});
+
+// Sending our earthquakes layer to the createMap function
+createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+    // Define lightmap and satellitemap layers
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openlightmap.org/copyright'>Openlightmap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/light-v10",
+      accessToken: API_KEY
+    });
+  
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openlightmap.org/\">Openlightmap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "satellite-v9",
+      accessToken: API_KEY
+    });
+  
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+      "Light Map": lightmap,
+      "Satellite Map": satellitemap
+    };
+  
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
+  
+    // Create our map, giving it the lightmap and earthquakes layers to display on load
+    var myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 5,
+      layers: [lightmap, earthquakes]
+    });
+  
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  }
+  
