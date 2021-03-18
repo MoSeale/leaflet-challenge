fe9534281cb31c2a756994586b5fa632fe192a11
MoSeale <57756845+MoSeale@users.noreply.github.com>
2021-03-17 21:17:56 -0400

    Summary
    Update links in index.html;
    Retrieve data from JSON in the logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -5,4 +5,27 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 d3.json(queryUrl, function(data) {
     // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
+    //console.log(data);
+
   });
+
+
+function createFeatures(earthquakeData) {
+    console.log(earthquakeData);
+// // Define a function we want to run once for each feature in the features array
+// // Give each feature a popup describing the place and time of the earthquake
+// function onEachFeature(feature, layer) {
+//     layer.bindPopup("<h3>" + feature.properties.place +
+//     "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+// }
+
+// // Create a GeoJSON layer containing the features array on the earthquakeData object
+// // Run the onEachFeature function once for each piece of data in the array
+// var earthquakes = L.geoJSON(earthquakeData, {
+//     onEachFeature: onEachFeature
+// });
+
+// // Sending our earthquakes layer to the createMap function
+// createMap(earthquakes);
+}
+
